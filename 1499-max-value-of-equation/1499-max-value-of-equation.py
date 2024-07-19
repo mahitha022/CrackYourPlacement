f700fcb222a8188dd82b3755afc967b57bc76537
mahitha022 <115865510+mahitha022@users.noreply.github.com>
2024-07-19 21:49:41 +0530

    Time: 1020 ms (27.89%), Space: 62.9 MB (42.18%) - LeetHub

---
+++ 1499-max-value-of-equation/1499-max-value-of-equation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
+        m=float('-inf')
+        last=0
+        i=0
+        while(i<len(points)):
+            if(i>=last):
+                last=i+1
+                
+            for j in range(last,len(points)):
+                diff=points[j][0]-points[i][0]
+                if(diff<=k):
+                    diff+=points[i][1]+points[j][1]
+                    if(diff>m):
+                        m=diff
+                        last=j
+                else:
+                    break
+            i+=1
+        return m
+    
+    
+        
+        
