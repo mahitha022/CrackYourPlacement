52186648e322d76400d66704f7a1fe750004c2fd
mahitha022 <115865510+mahitha022@users.noreply.github.com>
2024-07-22 19:26:24 +0530

    Create AGGRCOW-Aggressive-cows.py

---
+++ AGGRCOW-Aggressive-cows.py
@@ -0,0 +1,36 @@
+# your code goes here
+def check(mid):
+	cow=l[0]
+	count=1
+	for i in range(1,n):
+		if(l[i]-cow>=mid):
+			cow=l[i]
+			count+=1
+		if(count==c):
+			return True
+	return False
+	
+	
+	
+def binary(l,c):
+	low=l[0]
+	high=l[-1]-l[0]
+	res=-1
+	while(low<=high):
+		mid=(low+high)//2
+		if(check(mid)):
+			res=mid
+			low=mid+1
+		else:
+			high=mid-1
+	return res
+	
+	
+	
+	
+for _ in range(int(input())):
+	n,c=map(int,input().split())
+	l=[]
+	for __ in range(n):
+		l.append(int(input()))
+	print(binary(l,c))
