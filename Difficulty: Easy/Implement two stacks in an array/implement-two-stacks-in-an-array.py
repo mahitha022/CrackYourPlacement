14902444b42e73fd586a2703a7f493943a274917
mahitha022 <115865510+mahitha022@users.noreply.github.com>
2024-08-03 22:18:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement two stacks in an array/implement-two-stacks-in-an-array.py
@@ -0,0 +1,50 @@
+#User function Template for python3
+
+class TwoStacks:
+    def __init__(self):
+        self.l=[[],[]]
+
+    # Function to push an integer into stack 1
+    def push1(self, x):
+        self.l[0].append(x)
+
+    # Function to push an integer into stack 2
+    def push2(self, x):
+        self.l[1].append(x)
+
+    # Function to remove an element from top of stack 1
+    def pop1(self):
+        return self.l[0].pop() if self.l[0] else -1 
+
+    # Function to remove an element from top of stack 2
+    def pop2(self):
+        return self.l[1].pop() if self.l[1] else -1
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T = int(input())
+    while T > 0:
+        sq = TwoStacks()
+        Q = int(input())
+        while Q > 0:
+            query = list(map(int, input().split()))
+            if query[1] == 1:
+                if query[0] == 1:
+                    sq.push1(query[2])
+                elif query[0] == 2:
+                    sq.push2(query[2])
+            elif query[1] == 2:
+                if query[0] == 1:
+                    print(sq.pop1(), end=' ')
+                elif query[0] == 2:
+                    print(sq.pop2(), end=' ')
+            Q -= 1
+        print()
+        T -= 1
+
+# } Driver Code Ends
