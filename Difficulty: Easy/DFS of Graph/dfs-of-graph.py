6a0112574f940891ec8a70c3dc08492e0a6f29e7
mahitha022 <115865510+mahitha022@users.noreply.github.com>
2024-08-24 14:07:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to return a list containing the DFS traversal of the graph.
+    def dfsOfGraph(self, V, adj):
+        # code here
+        
+        def dfs(node):
+            visit[node]=True
+            res.append(node)
+            
+            for c in adj[node]:
+                if(not visit[c]):
+                    dfs(c)
+            
+            
+        res=[]
+        visit=[False]*V
+        dfs(0)
+        return res
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__ == '__main__':
+    T=int(input())
+    while T>0:
+        V,E=map(int,input().split())
+        adj=[[] for i in range(V+1)]
+        for i in range(E):
+            u,v=map(int,input().split())
+            adj[u].append(v)
+            adj[v].append(u)
+        ob=Solution()
+        ans=ob.dfsOfGraph(V,adj)
+        for i in range(len(ans)):
+            print(ans[i],end=" ")
+        print()
+        T-=1
+# } Driver Code Ends
